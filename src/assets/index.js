37d42abe4a9ff5e4ce5eb777d78776ab76e606f9
Antonino Privitera <antoninoprivitera@Antoninos-MBP.lan>
2025-09-21 11:41:44 +0200

    remove unused assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,38 +3,7 @@ import profilePic from "./profile-pic.jpg";
 import menu from "./menu.svg";
 import close from "./close.svg";
 import arrowUp from "./arrow-up.svg";
-import nitk from "./nitk.png";
-import cluboard from "./cluboard.png";
-import cash_flow from "./cash_flow.jpg";
-import ecell from "./ecell.png";
-import gdsc from "./gdsc.png";
-import iris from "./iris.png";
-import genesis from "./genesis.jpg";
-import portfolio from "./portfolio.jpg";
-import cdc from "./cdc.png";
-import chargeswap from "./chargeswap.png";
-import placeicon from "./place-icon.svg";
-import recruitment from "./recruitment.png";
-import huntly from "./huntly.png";
-import oracle from "./oracle.jpg";
-import comicify_ai from "./comicify-ai.png";
-import greentrust from "./green_trust.png";
-import averlon from "./averlon.jpg";
-import devfolio from "./devfolio.png";
-import pba from "./pba_logo.jpeg";
-import ethglobal from "./ethglobal.png";
-import polkadot from "./polkadot.jpeg";
-import lightspeed from "./lightspeed.png";
-import dennisivy from "./dennisivy.png";
-import manipal from "./manipal.png";
-import icon from "./icon.jpg";
-import ethforall from "./ethforall.png";
-import ethindia from "./ethindia.png";
 import placeholder from "./placeholder.svg";
-import globalAi from "./global-ai.jpeg";
-import khoj from "./khoj.jpg";
-import kosh from "./kosh.png";
-import polkadot_dev_cli from "./polkadot-dev-cli.png";
 
 // Experience Logos
 import criteo from "./experience_logos/criteo_logo.jpg";
@@ -70,39 +39,8 @@ export {
   menu,
   close,
   arrowUp,
-  nitk,
-  cluboard,
-  cash_flow,
-  gdsc,
-  iris,
-  ecell,
-  genesis,
-  portfolio,
-  cdc,
   profile_logo,
   profilePic,
-  chargeswap,
-  placeicon,
-  recruitment,
-  huntly,
-  oracle,
-  comicify_ai,
-  greentrust,
-  averlon,
-  devfolio,
-  pba,
-  ethglobal,
-  polkadot,
-  lightspeed,
-  dennisivy,
-  manipal,
-  icon,
-  ethforall,
-  ethindia,
-  globalAi,
-  khoj,
-  kosh,
-  polkadot_dev_cli,
   placeholder,
   criteo,
   head_of_design,
